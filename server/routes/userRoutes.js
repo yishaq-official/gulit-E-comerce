5c144bf481bb0e188421bc574f8898cde1a2292b
yishaq-official <yishaq.official@gmail.com>
2026-02-15 12:15:21 +0300

    setup for protected test for authentication middleware

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/authController');
+const { protect } = require('../middleware/authMiddleware');
+
 
 // Define the paths
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+// 🔒 Protected Route (Test)
+router.get('/profile', protect, (req, res) => {
+    res.json(req.user); // Should return Abebe's info if token is valid
+});
 
 module.exports = router;
