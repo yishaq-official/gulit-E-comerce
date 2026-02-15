8c217d51bc4a6cf3bd39478ff4fee45785172798
yishaq-official <yishaq.official@gmail.com>
2026-02-15 10:53:14 +0300

    middleware fix

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 
 //middlewares
-app.use(cors)
-app.use(express.json);
+app.use(cors())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, (err)=>{
