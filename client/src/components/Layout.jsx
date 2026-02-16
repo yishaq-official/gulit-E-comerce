a5e3ce20ad4c23583cd4e4935d55d9d7ba310ec2
yishaq-official <yishaq.official@gmail.com>
2026-02-16 18:56:03 +0300

    layout implementation to wrap all components

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
+      
+      {/* The Tibeb strip below the navbar */}
+      <div className="h-1.5 bg-tibeb-pattern bg-repeat-x w-full"></div>
+
+      <main className="flex-grow container mx-auto px-4 py-8">
+        {children}
+      </main>
+
+      {/* Another Tibeb strip above the footer */}
+      <div className="h-1.5 bg-tibeb-pattern bg-repeat-x w-full"></div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
