72df5b790f73c04983827ac934089f25a5e9306c
yishaq-official <yishaq.official@gmail.com>
2026-02-17 15:22:50 +0300

    data fetcher to reach products.

---
+++ client/src/store/slices/productsApiSlice.js
@@ -0,0 +1,24 @@
+import { apiSlice } from './apiSlice';
+
+export const productsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    // This hook will be used to fetch all products
+    getProducts: builder.query({
+      query: () => ({
+        url: '/api/products',
+      }),
+      keepUnusedDataFor: 5, // Caches data for 5 seconds
+      providesTags: ['Product'],
+    }),
+    // Hook for single product details
+    getProductDetails: builder.query({
+      query: (productId) => ({
+        url: `/api/products/${productId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+
+// RTK Query automatically generates these hooks based on the names above
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productsApiSlice;
