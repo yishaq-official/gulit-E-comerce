4d352bb89446d0c6bab76d2740f0b5144982efe1
yishaq-official <yishaq.official@gmail.com>
2026-02-17 20:03:54 +0300

    add profile update into user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, logoutUser, } = require('../controllers/authController');
+const { registerUser, loginUser, logoutUser, updateUserProfile} = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
 
@@ -13,6 +13,11 @@ router.post('/logout', logoutUser);
 router.get('/profile', protect, (req, res) => {
     res.json(req.user); // Should return Abebe's info if token is valid
 });
+
+
+router.route('/profile')
+    .get(protect, (req, res) => res.json(req.user)) // GET: View Profile
+    .put(protect, updateUserProfile);
 
 
 module.exports = router;
