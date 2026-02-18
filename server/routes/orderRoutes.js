ccf80944a7a371045eb698b632da39d62de4bb2e
yishaq-official <yishaq.official@gmail.com>
2026-02-18 20:40:26 +0300

    add update order to paid function to routes

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { addOrderItems, getOrderById, getMyOrders, } = require('../controllers/orderController');
+const { addOrderItems, getOrderById, getMyOrders, updateOrderToPaid } = require('../controllers/orderController');
 const { protect } = require('../middleware/authMiddleware');
 
 // All order routes are protected (You must be logged in to buy)
 router.route('/').post(protect, addOrderItems);
 router.route('/myorders').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 module.exports = router;
