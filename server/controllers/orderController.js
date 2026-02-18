f021b371ced4194e99e5a4a207f5a73a1591da55
yishaq-official <yishaq.official@gmail.com>
2026-02-18 20:38:21 +0300

    function added to set payed: true

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -76,8 +76,31 @@ const getMyOrders = async (req, res) => {
 };
 
 
+const updateOrderToPaid = async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    // Simulate payment result (like what Chapa would send back)
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.email_address,
+    };
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+};
+
 module.exports = {
     addOrderItems,
     getOrderById,
     getMyOrders,
+    updateOrderToPaid,
 };
