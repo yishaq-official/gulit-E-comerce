def134d1fe85235862caf7dfb848574987465161
yishaq-official <yishaq.official@gmail.com>
2026-02-20 22:31:43 +0300

    linking seller auth slice with the store.

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,11 +3,14 @@ import { apiSlice } from './slices/apiSlice';
 import cartSliceReducer from './slices/cartSlice';
 import authSliceReducer from './slices/authSlice';
 
+import sellerAuthReducer from './slices/sellerAuthSlice';
+
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducer,
     auth: authSliceReducer,
+    sellerAuth: sellerAuthReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
