c68be4c159b67b8701b08c932a690e6a1816d625
yishaq-official <yishaq.official@gmail.com>
2026-02-20 20:47:10 +0300

    route correction

--- client/src/components/SellerRoute.jsx
+++ client/src/components/SellerRoute.jsx
@@ -1,9 +1,16 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import React from 'react';
+import { Outlet, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const SellerRoute = () => {
   const { userInfo } = useSelector((state) => state.auth);
-  return userInfo && (userInfo.role === 'seller' || userInfo.role === 'admin') 
-    ? <Outlet /> : <Navigate to="/login" replace />;
+
+  // 👇 Check if they are logged in AND their role is 'seller'
+  return userInfo && userInfo.role === 'seller' ? (
+    <Outlet /> 
+  ) : (
+    <Navigate to="/login" replace />
+  );
 };
+
 export default SellerRoute;
