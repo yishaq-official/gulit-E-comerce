d2fe8453798e470e4d015fa5d27b238093c2091b
yishaq-official <yishaq.official@gmail.com>
2026-02-20 18:53:09 +0300

    add review route in products route

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -7,11 +7,15 @@ const {
 } = require('../controllers/productController');
 const { protect, seller } = require('../middleware/authMiddleware'); // Import our guards
 
+
 // Public Routes
 router.get('/', getProducts);
 router.get('/:id', getProductById);
 
 // Protected Route (Only Sellers can create!)
 router.post('/', protect, seller, createProduct);
+
+//create  review functionality for buyers after they recieve ordered item.
+router.route('/:id/reviews').post(protect, createProductReview);
 
 module.exports = router;
