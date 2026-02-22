80c4807c76041fdd966a443cb3dd5a9a4bce1aee
yishaq-official <yishaq.official@gmail.com>
2026-02-22 11:16:47 +0300

    share uploads folder to client side from backend.

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+      // 👇 ADD THIS SO IMAGES ROUTE TO THE BACKEND
+      '/uploads': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    }
+  }
 })
